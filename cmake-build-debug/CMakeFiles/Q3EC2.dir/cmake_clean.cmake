0e54cb2bc3f160cbca93765fffde7a866a9345a8
blue0print <67566815+blue0print@users.noreply.github.com>
2021-06-28 00:46:29 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Q3EC2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q3EC2.dir/Q3EC2.cpp.o"
+  "Q3EC2"
+  "Q3EC2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Q3EC2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
